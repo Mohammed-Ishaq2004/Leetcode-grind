3c25fde82d721c04b9e329a2e2783aa942687491
Mohammed-Ishaq2004 <mohammed2004ishaq@gmail.com>
2026-08-15 12:35:51 +0530

    Time: 2 ms (99.74%), Space: 43.3 MB (92.24%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int depth = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()) {
+
+            if(ch == '(') {
+                if(depth > 0) {
+                    sb.append(ch);
+                }
+                depth++;
+            }
+
+            else {
+                depth--;
+
+                if(depth > 0) {
+                    sb.append(ch);
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
