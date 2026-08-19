3d54515d92b099e22be1029b642e5a1b29b02972
Mohammed-Ishaq2004 <mohammed2004ishaq@gmail.com>
2026-08-19 16:23:22 +0530

    Time: 61 ms (62.87%), Space: 44.1 MB (73.63%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int beautySum(String s) {
+
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int[] freq = new int[26];
+
+            for (int j = i; j < s.length(); j++) {
+
+                // Add current character
+                freq[s.charAt(j) - 'a']++;
+
+                int max = 0;
+                int min = Integer.MAX_VALUE;
+
+                // Find max and min frequency
+                for (int k = 0; k < 26; k++) {
+
+                    if (freq[k] > 0) {
+                        max = Math.max(max, freq[k]);
+                        min = Math.min(min, freq[k]);
+                    }
+                }
+
+                sum += max - min;
+            }
+        }
+
+        return sum;
+    }
+}
